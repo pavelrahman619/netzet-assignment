95a16ade5689c227967d4b3440f9bf592220e9c8
pavelrahman619 <pavelrahman.dev@gmail.com>
2025-08-10 15:57:45 +0600

    add urbanist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Urbanist } from "next/font/google";
 import "./globals.css";
+
+
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${urbanist.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
